fe270488ad79a6433285a6c07df38d3c00d60942
Tyler Battle <tyler@battle.ca>
2021-10-21 00:44:51 -0700

    More strict mypy settings

--- src/stactools/worldpop/__init__.py
+++ src/stactools/worldpop/__init__.py
@@ -1,4 +1,5 @@
 import stactools.core
+from stactools.cli import Registry
 
 from stactools.worldpop.stac import create_collection, create_item
 
@@ -7,7 +8,7 @@ __all__ = ['create_collection', 'create_item']
 stactools.core.use_fsspec()
 
 
-def register_plugin(registry):
+def register_plugin(registry: Registry) -> None:
     from stactools.worldpop import commands
     registry.register_subcommand(commands.create_worldpop_command)
 
